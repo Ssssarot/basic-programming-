8bbdabf88ac683108014aab049bdae976c9ff0ee
Ssssarot <sarot.dkt48@gmail.com>
2024-07-19 18:39:47 +0900

    ประกาศตัวเเปร

--- ep.1/ep.1/index.ts
+++ ep.1/ep.1/index.ts
@@ -1,12 +1,12 @@
-const firstname : string ='sarot'
+ const firstname : string ='sarot'
  let y: number = 10
  y  = 20
- let อยากกลับบ้าน : boolean = false
+ let iWantToGoHome : boolean = false
  console.log(firstname)
  console.log(y)
- console.log(อยากกลับบ้าน)
+ console.log(iWantToGoHome)
  const a : number = '999'
  const b : number = '888'
- console.log (a==b)
+ console.log (a===b)
  console.log (a>b)
  console.log (a<=b)
